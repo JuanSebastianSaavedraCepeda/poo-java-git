50a972a4b12107aa89015fd4eeea08217747ba8c
JuanSebastianSaavedraCepeda <63920150+JuanSebastianSaavedraCepeda@users.noreply.github.com>
2023-06-11 10:59:47 -0500

    adicion de saldo a las cuentas

--- practica-clasesyobjetos/src/practica/clasesyobjetos/Cuenta.java
+++ practica-clasesyobjetos/src/practica/clasesyobjetos/Cuenta.java
@@ -4,12 +4,31 @@ package practica.clasesyobjetos;
 
 public class Cuenta {
      
-    private String nombre; //variable de instancia
+    private String nombre; //variable para el nombre de la cuenta
+    private double saldo; // variable para saldo
     
-   public Cuenta(String nombre)// el nombre del constructor es el nombre de la clase
+   public Cuenta(String nombre,double saldo)// el nombre del constructor es el nombre de la clase
  {
  this.nombre = nombre;
+ 
+ if(saldo > 0.0)//si el saldo es valido lo agrega a la variable saldo
+     this.saldo=saldo;
+   }
+   
+ // método que deposita (suma) sólo una cantidad válida al saldo
+public void depositar(double montoDeposito)
+ {
+if (montoDeposito > 0.0) // si el montoDeposito es válido
+ saldo = saldo + montoDeposito; // lo suma al saldo
+}
+
+
+// método que devuelve el saldo de la cuenta
+ public double obtenerSaldo()
+ {
+ return saldo;
  }
+ 
 
     
     
--- practica-clasesyobjetos/src/practica/clasesyobjetos/pruebaCuenta.java
+++ practica-clasesyobjetos/src/practica/clasesyobjetos/pruebaCuenta.java
@@ -9,19 +9,38 @@ public class pruebaCuenta {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //creamos un objeto scanner 
-        Scanner entrada= new Scanner(System.in);
+       
+         //Creamos un objeto cuenta y lo asignamos a mi cuenta
+         Cuenta cuenta1 = new Cuenta("Jane Green",50.13);
+         Cuenta cuenta2 = new Cuenta("John Blue",30.00);
         
-        //Creamos un objeto cuenta y lo asignamos a mi cuenta
-         Cuenta cuenta1 = new Cuenta("Jane Green");
-         Cuenta cuenta2 = new Cuenta("John Blue");
-        
-       System.out.printf("El nombre de cuenta1 es: %s%n", cuenta1.obtenerNombre());
-       System.out.printf("El nombre de cuenta2 es: %s%n", cuenta2.obtenerNombre());
-        
-        
-        
-        
+         // Mostramos el saldo inicial de cada objeto
+        System.out.printf("Saldo de %s: $%.2f%n", cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+        System.out.printf("Saldo de %s: $%.2f%n%n",cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+         
+         //c objeto scanner
+         Scanner entrada= new Scanner(System.in);
+         
+         
+         System.out.print("Escriba el monto a depositar para cuenta1: "); // indicador (promt)
+         double montoDeposito = entrada.nextDouble(); // obtiene entrada del usuario
+         System.out.printf("%nsumando %.2f al saldo de cuenta1%n%n",montoDeposito);
+         cuenta1.depositar(montoDeposito); // suma al saldo de cuenta1
+
+         
+         // muestra los saldos
+         System.out.printf("Saldo de %s: $%.2f%n",cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+         System.out.printf("Saldo de %s: $%.2f%n%n",cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+
+         
+         System.out.print("Escriba el monto a depositar para cuenta2: "); // indicador (promt)
+         montoDeposito = entrada.nextDouble(); // obtiene entrada del usuario
+         System.out.printf("%nsumando %.2f al saldo de cuenta2%n%n", montoDeposito);
+         cuenta2.depositar(montoDeposito); // suma al saldo de cuenta2
+
+         // muestra los saldos
+         System.out.printf("Saldo de %s: $%.2f%n",cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+         System.out.printf("Saldo de %s: $%.2f%n%n",cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+         
+        }
     }
-    
-}
